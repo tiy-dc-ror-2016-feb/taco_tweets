1bd7e57ae31a42aab243cf7800b6bfbce825fa8f
Russell Osborne <russell@theironyard.com>
2016-03-30 11:02:00 -0400

    Add basic setup

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source "https://rubygems.org"
+
+gem "activerecord"
+gem "sinatra"
+
+group :test do
+  gem "pry"
+  gem "minitest"
+  gem "rack-test"
+end
---
+++ tests/app_test.rb
@@ -0,0 +1,11 @@
+require 'minitest/autorun'
+require 'rack/test'
+require 'pry'
+
+class AppTest < Minitest::Test
+
+  def test_tacos_are_still_tacos
+    assert true
+  end
+
+end
